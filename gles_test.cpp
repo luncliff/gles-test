13dbcad2375da4720e56914a4bd2c3bcf6f51772
luncliff <luncliff@gmail.com>
2022-09-12 17:13:52 +0900

    appveyor: setup vulkan with installer
    * use build cache for vcpkg libraries
    * exclude bext-ut

--- gles_test.cpp
+++ gles_test.cpp
@@ -1,11 +1,9 @@
-// #include <boost/ut.hpp>
 #include <cstdio>
 #include <filesystem>
 #include <spdlog/sinks/stdout_sinks.h>
 #include <spdlog/spdlog.h>
 
 namespace fs = std::filesystem;
-namespace testing = boost::ut;
 
 auto make_logger(const char* name, FILE* fout) noexcept(false) {
     using mutex_t = spdlog::details::console_nullmutex;
@@ -13,12 +11,12 @@ auto make_logger(const char* name, FILE* fout) noexcept(false) {
     return std::make_shared<spdlog::logger>(name, std::make_shared<sink_t>(fout));
 }
 
-int main(int argc, char* argv[]) {
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
     auto logger = make_logger("test", stdout);
     logger->set_pattern("%T.%e [%L] %8t %v");
     logger->set_level(spdlog::level::level_enum::debug);
     spdlog::set_default_logger(logger);
     auto cwd = fs::current_path();
-    spdlog::debug("cwd: {}", cwd.generic_u8string());
+    spdlog::debug("cwd: {}", cwd.generic_string());
     return 0;
 }
